5e7ec3f8b80d70993ced1e7896a3af9da0359474
Megan Thomas <methomas@adobe.com>
2024-06-17 18:02:06 -0700

    split tests

--- test/blocks/aside/aside-new.test.js
+++ test/blocks/aside/aside-new.test.js
@@ -7,49 +7,47 @@ const splitBody = await readFile({ path: './mocks/split.html' });
 
 describe('aside', () => {
   describe('standard', () => {
-    document.body.innerHTML = standardBody;
-    const blocks = document.querySelectorAll('.aside');
-    blocks.forEach((el) => init(el));
+    before(() => {
+      document.body.innerHTML = standardBody;
+      const blocks = document.querySelectorAll('.aside');
+      blocks.forEach((el) => init(el));
+    });
 
-    describe('default', () => {
+    it('allows a background color', () => {
       const el = document.querySelector('#test-default');
-      const el2 = document.querySelector('#test-default-2');
+      expect(window.getComputedStyle(el)?.backgroundColor).to.equal('rgb(238, 238, 238)');
+    });
 
-      it('allows a background color', () => {
-        expect(window.getComputedStyle(el)?.backgroundColor).to.equal('rgb(238, 238, 238)');
-      });
+    it('allows a background image', () => {
+      expect(document.querySelector('#test-default-2 .background img')).to.exist;
+    });
 
-      it('allows a background image', () => {
-        expect(el2.querySelector('.background img')).to.exist;
-      });
+    it('allows an icon image', () => {
+      expect(document.querySelector('#test-default .icon-area img')).to.exist;
+    });
 
-      it('allows an icon image', () => {
-        expect(el.querySelector('.icon-area img')).to.exist;
-      });
+    it('has Detail M by default', () => {
+      expect(document.querySelector('#test-default .detail-m')).to.exist;
+    });
 
-      it('has a medium detail', () => {
-        expect(el.querySelector('.detail-m')).to.exist;
-      });
+    it('has Heading XL by default', () => {
+      expect(document.querySelector('#test-default .heading-xl')).to.exist;
+    });
 
-      it('has a xl heading', () => {
-        expect(el.querySelector('.heading-xl')).to.exist;
-      });
+    it('has Body S by default', () => {
+      expect(document.querySelector('#test-default p.body-s')).to.exist;
+    });
 
-      it('has a small body', () => {
-        expect(el.querySelector('p.body-s')).to.exist;
-      });
+    it('allows a cta', () => {
+      expect(document.querySelector('#test-default .action-area .con-button')).to.exist;
+    });
 
-      it('has a cta', () => {
-        expect(el.querySelector('.action-area .con-button')).to.exist;
-      });
+    it('allows supplemental text', () => {
+      expect(document.querySelector('#test-default .supplemental-text')).to.exist;
+    });
 
-      it('allows supplemental text', () => {
-        expect(el.querySelector('.supplemental-text')).to.exist;
-      });
-
-      it('allows a foreground image', () => {
-        expect(el.querySelector('.foreground .image img')).to.exist;
-      });
+    it('allows a foreground image', () => {
+      expect(document.querySelector('#test-default .foreground .image img')).to.exist;
     });
 
     it('allows text overrides', () => {
@@ -59,9 +57,9 @@ describe('aside', () => {
       expect(el.querySelector('p.body-m')).to.exist;
     });
 
-    it('allows large title', () => {
+    it('allows Title L to override Detail', () => {
       const el = document.querySelector('#test-title');
-      expect(el.querySelector('.title-l')).to.exist;
+      expect(el.querySelector('.detail-m.title-l')).to.exist;
     });
 
     it('allows an avatar', () => {
@@ -72,52 +70,67 @@ describe('aside', () => {
     // it('has a product lockup'); // To Do
   });
 
-  // describe('split', () => {
-  //   document.body.innerHTML = splitBody;
-  //   const blocks = document.querySelectorAll('.aside');
-  //   blocks.forEach((el) => init(el));
+  describe('split', () => {
+    before(() => {
+      document.body.innerHTML = splitBody;
+      const blocks = document.querySelectorAll('.aside');
+      blocks.forEach((el) => init(el));
+    });
 
-  //   describe('default', () => {
-  //     const el = document.querySelector('#test-default');
-  //     const el2 = document.querySelector('#test-default-2');
+    it('allows a background color', () => {
+      const el = document.querySelector('#test-default');
+      expect(window.getComputedStyle(el)?.backgroundColor).to.equal('rgb(30, 30, 30)');
+    });
 
-  //     it('allows a background color', () => {
-  //       expect(el.style.backroundColor).to.equal('#1e1e1e');
-  //     });
+    it('allows an icon image', () => {
+      expect(document.querySelector('#test-default .icon-area img')).to.exist;
+    });
 
-  //     // it('allows a background image', () => {
-  //     //   expect(el2.querySelector('.background img')).to.exist;
-  //     // });
+    it('has Detail M by default', () => {
+      expect(document.querySelector('#test-default .detail-m')).to.exist;
+    });
 
-  //     it('allows an icon image', () => {
-  //       expect(el.querySelector('.icon-area img')).to.exist;
-  //     });
+    it('has Heading XL by default', () => {
+      expect(document.querySelector('#test-default .heading-xl')).to.exist;
+    });
 
-  //     it('has a medium detail', () => {
-  //       expect(el.querySelector('.detail-m')).to.exist;
-  //     });
+    it('has Body S by default', () => {
+      expect(document.querySelector('#test-default p.body-s')).to.exist;
+    });
 
-  //     it('has a xl heading', () => {
-  //       expect(el.querySelector('.heading-xl')).to.exist;
-  //     });
+    it('allows icon stack', () => {
+      expect(document.querySelector('#test-default .icon-stack-area')).to.exist;
+    });
 
-  //     it('has a small body', () => {
-  //       expect(el.querySelector('p.body-s')).to.exist;
-  //     });
+    it('allows a cta', () => {
+      expect(document.querySelector('#test-default .action-area .con-button')).to.exist;
+    });
 
-  //     it('has a cta', () => {
-  //       expect(el.querySelector('.action-area .con-button')).to.exist;
-  //     });
+    it('allows supplemental text', () => {
+      expect(document.querySelector('#test-default .supplemental-text')).to.exist;
+    });
 
-  //     it('allows supplemental text', () => {
-  //       expect(el.querySelector('.supplemental-text')).to.exist;
-  //     });
+    it('allows a split image', () => {
+      expect(document.querySelector('#test-default .split-image img')).to.exist;
+    });
 
-  //     // it('allows a foreground image', () => {
-  //     //   expect(el.querySelector('.foreground .image img')).to.exist;
-  //     // });
+    it('allows text overrides', () => {
+      const el = document.querySelector('#test-text-overrides');
+      expect(el.querySelector('.detail-l')).to.exist;
+      expect(el.querySelector('.heading-l')).to.exist;
+      expect(el.querySelector('p.body-m')).to.exist;
+    });
 
-  //     it('allows icon stack');
-  //   });
-  // });
+    it('allows Title L to override Detail', () => {
+      const el = document.querySelector('#test-title');
+      expect(el.querySelector('.detail-m.title-l')).to.exist;
+    });
+
+    it('allows an avatar', () => {
+      const el = document.querySelector('#test-avatar');
+      expect(el.querySelector('.avatar-area img')).to.exist;
+    });
+
+    // it('has a product lockup'); // To Do
+  });
 });
